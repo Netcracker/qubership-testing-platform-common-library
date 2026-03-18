c96ee78cac6effa2556f65f49412d2917fb4c555
kagw95 <aleksandr.kapustin.qubership@gmail.com>
2026-03-18 16:41:57 +0400

    refactor: org.hamcrest usages are replaced to org.junit.jupiter.api. Dependency org.hamcrest:hamcrest-junit:2.0.0.0 is removed. Dependency org.junit.vintage:junit-vintage-engine is made managed by Spring Boot.

--- qubership-atp-common-logging/src/test/java/org/qubership/atp/common/logging/utils/RegexUtilTest.java
+++ qubership-atp-common-logging/src/test/java/org/qubership/atp/common/logging/utils/RegexUtilTest.java
@@ -1,5 +1,5 @@
 /*
- * # Copyright 2024-2025 NetCracker Technology Corporation
+ * # Copyright 2024-2026 NetCracker Technology Corporation
  * #
  * # Licensed under the Apache License, Version 2.0 (the "License");
  * # you may not use this file except in compliance with the License.
@@ -17,12 +17,9 @@
 package org.qubership.atp.common.logging.utils;
 
 import static com.google.common.collect.Lists.newArrayList;
-import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
-import static java.util.Collections.unmodifiableList;
 import static java.util.stream.Collectors.toMap;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -84,10 +81,10 @@ public class RegexUtilTest {
      */
     @BeforeEach
     public void setUp() {
-        targetCollection = unmodifiableList(asList(BEARER_HEADER, X_REQUEST_ID_HEADER, ZIPKIN_TRACE_ID_HEADER));
+        targetCollection = List.of(BEARER_HEADER, X_REQUEST_ID_HEADER, ZIPKIN_TRACE_ID_HEADER);
         targetMap = targetCollection.stream().collect(toMap(String::toString, Collections::singletonList));
         expectedResult = singletonList(ZIPKIN_TRACE_ID_HEADER);
-        patterns = unmodifiableList(asList(Pattern.compile(BEARER_HEADER), Pattern.compile("\\D*Request\\D*")));
+        patterns = List.of(Pattern.compile(BEARER_HEADER), Pattern.compile("\\D*Request\\D*"));
     }
 
     /**
@@ -132,7 +129,7 @@ public class RegexUtilTest {
     @Test
     public void testRemoveByRegexPatternsShouldRemoveHeadersWhenPatternsAreNotEmpty() {
         Collection<String> result = removeByRegexPatterns(targetCollection, patterns);
-        assertThat(result, equalTo(expectedResult));
+        assertEquals(expectedResult, result);
     }
 
     /**
@@ -141,7 +138,7 @@ public class RegexUtilTest {
     @Test
     public void testRemoveByRegexPatternsShouldExistAllHeadersWhenPatternsAreEmpty() {
         Collection<String> result = removeByRegexPatterns(targetCollection, newArrayList());
-        assertThat(result, equalTo(targetCollection));
+        assertEquals(targetCollection, result);
     }
 
     /**
@@ -150,7 +147,7 @@ public class RegexUtilTest {
     @Test
     public void testRemoveByKeyRegexPatternsShouldRemoveHeadersWhenPatternsAreNotEmpty() {
         Map<String, Collection<String>> result = removeByKeyRegexPatterns(targetMap, patterns);
-        assertThat(newArrayList(result.keySet()), equalTo(expectedResult));
+        assertEquals(expectedResult, newArrayList(result.keySet()));
     }
 
     /**
@@ -159,6 +156,6 @@ public class RegexUtilTest {
     @Test
     public void testRemoveByKeyRegexPatternsShouldExistAllHeadersWhenPatternsAreEmpty() {
         Map<String, Collection<String>> result = removeByKeyRegexPatterns(targetMap, newArrayList());
-        assertThat(result, equalTo(targetMap));
+        assertEquals(targetMap, result);
     }
 }
